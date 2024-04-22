206c801296d1cda6f07b87ac18d9342d56be516f
actsolucoesparapessoas <129021588+actsolucoesparapessoas@users.noreply.github.com>
2024-04-22 05:15:30 -0300

    Update Cadastrar_Disciplinas.py

--- pages/Cadastrar_Disciplinas.py
+++ pages/Cadastrar_Disciplinas.py
@@ -42,11 +42,11 @@ def inicializar():
 
 col0, col1, col2 = st.columns(3)       
 with col0:
-    st.page_link("https://ctrltarefas.streamlit.app/", label="Cadastrar Tarefas", icon="📌") 
+    st.page_link("https://gestaodetarefas.streamlit.app/", label="Cadastrar Tarefas", icon="📌") 
 with col1:
-    st.page_link("https://ctrltarefas.streamlit.app/Cadastrar_Usuario", label="Cadastrar Usuários", icon="👨‍💼")
+    st.page_link("https://gestaodetarefas.streamlit.app/Cadastrar_Usuario", label="Cadastrar Usuários", icon="👨‍💼")
 with col2:
-    st.page_link("https://ctrltarefas.streamlit.app/Painel_Tarefas", label="Painel de Tarefas", icon="📊") 
+    st.page_link("https://gestaodetarefas.streamlit.app/Painel_Tarefas", label="Painel de Tarefas", icon="📊") 
 
 tab1, tab2 = st.tabs(["Cadastrar", "EXIBIR"])
 with tab1:    
